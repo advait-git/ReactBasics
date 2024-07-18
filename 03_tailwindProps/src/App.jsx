2f904d6a179c0d074f9ed388b69f50248a2cdb95
advait-git <advaittiwari2@gmail.com>
2024-07-19 00:28:59 +0530

    props basic react

---
+++ 03_tailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './components/card'
+function App() {
+
+  return (
+   <div>
+    <Card username="Akshita" btnText="Click me"/>
+    <Card/>
+    
+   </div>
+  )
+}
+
+export default App
